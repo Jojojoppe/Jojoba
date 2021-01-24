127a3abdbc5f999ba12fbbccc1783123ec6cfe02
Joppe Blondel <joppe@blondel.nl>
2021-01-24 12:45:33 +0100

    Added vermilion to repo
    Signed-off-by: Joppe Blondel <joppe@blondel.nl>

--- libjojoba/core/src/jojoba.cpp
+++ libjojoba/core/src/jojoba.cpp
@@ -1,6 +1,12 @@
 #include <jojoba/jojoba.hpp>
+#include <vermilion/vermilion.hpp>
 
 #include <cstdio>
 
 void Jojoba::Core::test(){
+    VmInstance instance(nullptr, nullptr, nullptr, Vermilion::Core::WindowCallbackFunctions{});
+    while(instance.shouldClose()){
+        instance.startRender();
+        instance.endRender({});
+    }
 }
